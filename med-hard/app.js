5bcf42505a9702a2b9f2d0992e4c08a1587155f3
ashley-woodton <ashley.woodton@gmail.com>
2020-12-15 20:47:02 -0500

    firs commit

---
+++ med-hard/app.js
@@ -0,0 +1,102 @@
+// GET: localhost:5060/employees/<employeeID> = Returns json with the information from that specific employee.
+// POST:localhost:5060/employees/ = Inserts new employee into your data. GET::myendpointname.com/employees = Returns json with information from all employees.
+// PUT:localhost:5060/employees/<employeeID> = Updates information for specified employee.
+// DELETE:localhost:5060/employees/<employeeID> = Removes the employee with that ID from the data.
+
+
+const express = require('express');
+const app = express();
+const data = require('./public/database.json');
+const Joi = require('joi');
+
+app.use(express.json());
+
+app.get('/employees', (req,res) => {
+    if(!data){
+        res.status(404).send('Could not find information')
+    }
+    res.send(data)
+});
+
+app.get('/employees/:id', (req,res) => {
+    const findEmployee = data.employees.find(employee =>{
+        return parseInt(req.params.id) === employee.id
+    });
+
+    if(!findEmployee){
+        res.status(404).send('Employee was not found');
+    }
+    res.send(findEmployee);
+});
+
+//Add Employee
+app.post("/employees", (req, res) => {
+    const { error } = validateEmployee(req.body);
+    if (error) return res.status(400).send(result.error.details[0].message);
+ 
+    const addWorker = {
+      id: data.employees.length + 1,
+      name: req.body.name,
+      salary: req.body.salary,
+      department: req.body.department,
+    };
+    if (!addWorker) {
+      res.status(404).send("Could not find information");
+    }
+    data.employees.push(addWorker);
+    res.send(addWorker);
+    return;
+  });
+
+// Update Employee
+  app.put("/employees/:id", (req, res) => {
+    const employee = data.employees.find(employee =>{
+        return parseInt(req.params.id) === employee.id
+    });
+
+    if(!employee) {
+        return res.status(404).send('Employee was not found');
+    }
+
+    const { error } = validateEmployee(req.body);
+    if (error) return res.status(400).send(result.error.details[0].message);
+
+        employee.name = req.body.name;
+        employee.salary = req.body.salary;
+        employee.department = req.body.department;
+        
+      res.send(employee)
+  });
+
+//Delete Employee
+  app.delete("/employees/:id", (req, res) => {
+    const employee = data.employees.find(employee =>{
+        return parseInt(req.params.id) === employee.id
+    });
+
+    if(!employee) return res.status(404).send('Employee was not found');
+
+
+    const index = data.employees.indexOf(employee);
+    data.employees.splice(index, 1);
+   
+    return res.send(employee);
+
+  });
+
+// Validation function
+const validateEmployee = (employee) => {
+    const schema = {
+        name: Joi.string().min(2).required(),
+        salary: Joi.number().required(),
+        department: Joi.string().min(2).required(),
+      };
+      return Joi.validate(employee, schema);
+      
+}
+
+
+
+
+const port = process.env.PORT || 5010;
+app.listen(port);
